e7fd849aa227eb1a35b6799aa1f1b532560faac3
andypicke <andypicke@gmail.com>
2016-10-12 10:21:16 -0600

    update

--- TryLeafletAirports.R
+++ TryLeafletAirports.R
@@ -7,8 +7,9 @@ library(weatherData)
 cont_us <- subset(USAirportWeatherStations,State!="AK" & State!="MP" & State!="PR" & State!="HI" & State!="VI" & State!="GU")
 
 m <- leaflet() %>%
+        setMaxBounds(-125,23,-67,50)%>%
         addTiles() %>%  # Add default OpenStreetMap map tiles
-addMarkers(lng=cont_us$Lon,lat=cont_us$Lat,popup=cont_us$Station,clusterOptions = markerClusterOptions())
+addCircleMarkers(lng=cont_us$Lon,lat=cont_us$Lat,popup=cont_us$Station,radius=2,fill=TRUE,color="grey")#,clusterOptions = markerClusterOptions())
 #addCircles(lng=cont_us$Lon,lat=cont_us$Lat)
 m  # Print the map
 
